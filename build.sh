ceea61da5b42eeb796bc410a3786643f0f9e1e5a
Xiaoyu <xiaoyusama@outlook.com>
2019-09-26 09:38:09 -0400

    revise build

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir "./apiv4"
 echo "protoc"
 "./bin/protoc" -I="." \
 --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" \
---js_out="import_style=commonjs,binary:./(dist)-apiv4" --ts_out="./(dist)-apiv4" \
+--js_out="import_style=commonjs,binary:./apiv4" --ts_out="./apiv4" \
 "apiv4.proto" \
 "./include/page.proto" \
 "./include/revision.proto" \
