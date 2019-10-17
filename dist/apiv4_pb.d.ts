97327c8859b51cb5c1027c68507f137ea2cdde2a
xiaoyu <xiaoyu@xiaoyunoMacBook.local>
2019-10-17 12:18:25 -0400

    + event
    Signed-off-by: xiaoyu <xiaoyu@xiaoyunoMacBook.local>

---
+++ dist/apiv4_pb.d.ts
@@ -0,0 +1,248 @@
+// package: 
+// file: apiv4.proto
+
+import * as jspb from "google-protobuf";
+import * as include_support_pb from "./include/support_pb";
+import * as include_page_pb from "./include/page_pb";
+import * as include_feed_pb from "./include/feed_pb";
+import * as include_user_pb from "./include/user_pb";
+
+export class Api_request_wrapper extends jspb.Message {
+  getMessageVersion(): string;
+  setMessageVersion(value: string): void;
+
+  hasCredential(): boolean;
+  clearCredential(): void;
+  getCredential(): include_support_pb.Credential | undefined;
+  setCredential(value?: include_support_pb.Credential): void;
+
+  hasPowReply(): boolean;
+  clearPowReply(): void;
+  getPowReply(): include_support_pb.Proof_of_work_produced | undefined;
+  setPowReply(value?: include_support_pb.Proof_of_work_produced): void;
+
+  getTestfield(): number;
+  setTestfield(value: number): void;
+
+  hasPageRenderingElements(): boolean;
+  clearPageRenderingElements(): void;
+  getPageRenderingElements(): include_page_pb.Request_page_rendering_elements_by_name | undefined;
+  setPageRenderingElements(value?: include_page_pb.Request_page_rendering_elements_by_name): void;
+
+  hasPageMetadata(): boolean;
+  clearPageMetadata(): void;
+  getPageMetadata(): include_page_pb.Request_page_metadata_by_name | undefined;
+  setPageMetadata(value?: include_page_pb.Request_page_metadata_by_name): void;
+
+  hasCreateOrEditPage(): boolean;
+  clearCreateOrEditPage(): void;
+  getCreateOrEditPage(): include_page_pb.Request_create_or_edit_page | undefined;
+  setCreateOrEditPage(value?: include_page_pb.Request_create_or_edit_page): void;
+
+  hasFeedRenderingElements(): boolean;
+  clearFeedRenderingElements(): void;
+  getFeedRenderingElements(): include_feed_pb.Request_feed_rendering_elements_by_name | undefined;
+  setFeedRenderingElements(value?: include_feed_pb.Request_feed_rendering_elements_by_name): void;
+
+  hasFeedMetadata(): boolean;
+  clearFeedMetadata(): void;
+  getFeedMetadata(): include_feed_pb.Request_feed_metadata_by_name | undefined;
+  setFeedMetadata(value?: include_feed_pb.Request_feed_metadata_by_name): void;
+
+  hasSignInPwdNaive(): boolean;
+  clearSignInPwdNaive(): void;
+  getSignInPwdNaive(): include_user_pb.Obtain_credential_by_password_naive_attempt | undefined;
+  setSignInPwdNaive(value?: include_user_pb.Obtain_credential_by_password_naive_attempt): void;
+
+  hasSignInHmacTotp(): boolean;
+  clearSignInHmacTotp(): void;
+  getSignInHmacTotp(): include_user_pb.Obtain_credential_2fa_hmac_totp_code | undefined;
+  setSignInHmacTotp(value?: include_user_pb.Obtain_credential_2fa_hmac_totp_code): void;
+
+  hasSignInMailOtp0(): boolean;
+  clearSignInMailOtp0(): void;
+  getSignInMailOtp0(): include_user_pb.Obtain_credential_mail_otp_step_0_response | undefined;
+  setSignInMailOtp0(value?: include_user_pb.Obtain_credential_mail_otp_step_0_response): void;
+
+  hasSignInMailOtp1(): boolean;
+  clearSignInMailOtp1(): void;
+  getSignInMailOtp1(): include_user_pb.Obtain_credential_mail_otp_step_1_response | undefined;
+  setSignInMailOtp1(value?: include_user_pb.Obtain_credential_mail_otp_step_1_response): void;
+
+  hasCreateUserNaive(): boolean;
+  clearCreateUserNaive(): void;
+  getCreateUserNaive(): include_user_pb.Request_create_user_naive_attempt | undefined;
+  setCreateUserNaive(value?: include_user_pb.Request_create_user_naive_attempt): void;
+
+  hasApplyAnonymousUser(): boolean;
+  clearApplyAnonymousUser(): void;
+  getApplyAnonymousUser(): include_user_pb.Request_apply_anonymous_user | undefined;
+  setApplyAnonymousUser(value?: include_user_pb.Request_apply_anonymous_user): void;
+
+  getActualMessageCase(): Api_request_wrapper.ActualMessageCase;
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Api_request_wrapper.AsObject;
+  static toObject(includeInstance: boolean, msg: Api_request_wrapper): Api_request_wrapper.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Api_request_wrapper, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Api_request_wrapper;
+  static deserializeBinaryFromReader(message: Api_request_wrapper, reader: jspb.BinaryReader): Api_request_wrapper;
+}
+
+export namespace Api_request_wrapper {
+  export type AsObject = {
+    messageVersion: string,
+    credential?: include_support_pb.Credential.AsObject,
+    powReply?: include_support_pb.Proof_of_work_produced.AsObject,
+    testfield: number,
+    pageRenderingElements?: include_page_pb.Request_page_rendering_elements_by_name.AsObject,
+    pageMetadata?: include_page_pb.Request_page_metadata_by_name.AsObject,
+    createOrEditPage?: include_page_pb.Request_create_or_edit_page.AsObject,
+    feedRenderingElements?: include_feed_pb.Request_feed_rendering_elements_by_name.AsObject,
+    feedMetadata?: include_feed_pb.Request_feed_metadata_by_name.AsObject,
+    signInPwdNaive?: include_user_pb.Obtain_credential_by_password_naive_attempt.AsObject,
+    signInHmacTotp?: include_user_pb.Obtain_credential_2fa_hmac_totp_code.AsObject,
+    signInMailOtp0?: include_user_pb.Obtain_credential_mail_otp_step_0_response.AsObject,
+    signInMailOtp1?: include_user_pb.Obtain_credential_mail_otp_step_1_response.AsObject,
+    createUserNaive?: include_user_pb.Request_create_user_naive_attempt.AsObject,
+    applyAnonymousUser?: include_user_pb.Request_apply_anonymous_user.AsObject,
+  }
+
+  export enum ActualMessageCase {
+    ACTUAL_MESSAGE_NOT_SET = 0,
+    PAGE_RENDERING_ELEMENTS = 11,
+    PAGE_METADATA = 12,
+    CREATE_OR_EDIT_PAGE = 13,
+    FEED_RENDERING_ELEMENTS = 21,
+    FEED_METADATA = 22,
+    SIGN_IN_PWD_NAIVE = 31,
+    SIGN_IN_HMAC_TOTP = 33,
+    SIGN_IN_MAIL_OTP_0 = 34,
+    SIGN_IN_MAIL_OTP_1 = 35,
+    CREATE_USER_NAIVE = 36,
+    APPLY_ANONYMOUS_USER = 37,
+  }
+}
+
+export class Api_response_wrapper extends jspb.Message {
+  getMessageVersion(): string;
+  setMessageVersion(value: string): void;
+
+  getExecutionOk(): boolean;
+  setExecutionOk(value: boolean): void;
+
+  getError(): Api_response_wrapper.Broad_error_typeMap[keyof Api_response_wrapper.Broad_error_typeMap];
+  setError(value: Api_response_wrapper.Broad_error_typeMap[keyof Api_response_wrapper.Broad_error_typeMap]): void;
+
+  getMissionSpecificError(): string;
+  setMissionSpecificError(value: string): void;
+
+  hasPageRenderingElements(): boolean;
+  clearPageRenderingElements(): void;
+  getPageRenderingElements(): include_page_pb.Page_with_rendering_elements | undefined;
+  setPageRenderingElements(value?: include_page_pb.Page_with_rendering_elements): void;
+
+  hasPageMetadata(): boolean;
+  clearPageMetadata(): void;
+  getPageMetadata(): include_page_pb.Page_metadata | undefined;
+  setPageMetadata(value?: include_page_pb.Page_metadata): void;
+
+  hasFeedRenderingElements(): boolean;
+  clearFeedRenderingElements(): void;
+  getFeedRenderingElements(): include_feed_pb.Feed_with_rendering_elements | undefined;
+  setFeedRenderingElements(value?: include_feed_pb.Feed_with_rendering_elements): void;
+
+  hasFeedMetadata(): boolean;
+  clearFeedMetadata(): void;
+  getFeedMetadata(): include_feed_pb.Feed_metadata | undefined;
+  setFeedMetadata(value?: include_feed_pb.Feed_metadata): void;
+
+  hasSignInHmacTotpRequired(): boolean;
+  clearSignInHmacTotpRequired(): void;
+  getSignInHmacTotpRequired(): include_user_pb.Reply_2fa_hmac_totp_required_to_obtain_credential | undefined;
+  setSignInHmacTotpRequired(value?: include_user_pb.Reply_2fa_hmac_totp_required_to_obtain_credential): void;
+
+  hasSignInMailTotpStep0(): boolean;
+  clearSignInMailTotpStep0(): void;
+  getSignInMailTotpStep0(): include_user_pb.Reply_2fa_mail_otp_step_0_verify_email_addr | undefined;
+  setSignInMailTotpStep0(value?: include_user_pb.Reply_2fa_mail_otp_step_0_verify_email_addr): void;
+
+  hasSignInMailTotpStep1(): boolean;
+  clearSignInMailTotpStep1(): void;
+  getSignInMailTotpStep1(): include_user_pb.Reply_2fa_mail_otp_step_1_mail_sent | undefined;
+  setSignInMailTotpStep1(value?: include_user_pb.Reply_2fa_mail_otp_step_1_mail_sent): void;
+
+  hasCredentialGranted(): boolean;
+  clearCredentialGranted(): void;
+  getCredentialGranted(): include_user_pb.Response_credential_granted | undefined;
+  setCredentialGranted(value?: include_user_pb.Response_credential_granted): void;
+
+  hasUserCreated(): boolean;
+  clearUserCreated(): void;
+  getUserCreated(): include_user_pb.Response_user_created | undefined;
+  setUserCreated(value?: include_user_pb.Response_user_created): void;
+
+  hasAnonymousUserCreated(): boolean;
+  clearAnonymousUserCreated(): void;
+  getAnonymousUserCreated(): include_user_pb.Response_user_created | undefined;
+  setAnonymousUserCreated(value?: include_user_pb.Response_user_created): void;
+
+  getActualMessageCase(): Api_response_wrapper.ActualMessageCase;
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Api_response_wrapper.AsObject;
+  static toObject(includeInstance: boolean, msg: Api_response_wrapper): Api_response_wrapper.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Api_response_wrapper, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Api_response_wrapper;
+  static deserializeBinaryFromReader(message: Api_response_wrapper, reader: jspb.BinaryReader): Api_response_wrapper;
+}
+
+export namespace Api_response_wrapper {
+  export type AsObject = {
+    messageVersion: string,
+    executionOk: boolean,
+    error: Api_response_wrapper.Broad_error_typeMap[keyof Api_response_wrapper.Broad_error_typeMap],
+    missionSpecificError: string,
+    pageRenderingElements?: include_page_pb.Page_with_rendering_elements.AsObject,
+    pageMetadata?: include_page_pb.Page_metadata.AsObject,
+    feedRenderingElements?: include_feed_pb.Feed_with_rendering_elements.AsObject,
+    feedMetadata?: include_feed_pb.Feed_metadata.AsObject,
+    signInHmacTotpRequired?: include_user_pb.Reply_2fa_hmac_totp_required_to_obtain_credential.AsObject,
+    signInMailTotpStep0?: include_user_pb.Reply_2fa_mail_otp_step_0_verify_email_addr.AsObject,
+    signInMailTotpStep1?: include_user_pb.Reply_2fa_mail_otp_step_1_mail_sent.AsObject,
+    credentialGranted?: include_user_pb.Response_credential_granted.AsObject,
+    userCreated?: include_user_pb.Response_user_created.AsObject,
+    anonymousUserCreated?: include_user_pb.Response_user_created.AsObject,
+  }
+
+  export interface Broad_error_typeMap {
+    NO_INFO: 0;
+    INSUFFICIENT_PERMISSION: 1;
+    INVALID_MESSAGE: 2;
+    DNE: 3;
+    MISSION_SPECIFIC: 4;
+    PROCEDURE_NOT_SUPPORTED: 5;
+    MESSAGE_VERSION_TOO_OLD: 6;
+    MESSAGE_VERSION_TOO_NEW: 7;
+    POW_REQUIRED_TO_CONTINUE: 8;
+  }
+
+  export const Broad_error_type: Broad_error_typeMap;
+
+  export enum ActualMessageCase {
+    ACTUAL_MESSAGE_NOT_SET = 0,
+    PAGE_RENDERING_ELEMENTS = 11,
+    PAGE_METADATA = 12,
+    FEED_RENDERING_ELEMENTS = 21,
+    FEED_METADATA = 22,
+    SIGN_IN_HMAC_TOTP_REQUIRED = 41,
+    SIGN_IN_MAIL_TOTP_STEP_0 = 42,
+    SIGN_IN_MAIL_TOTP_STEP_1 = 43,
+    CREDENTIAL_GRANTED = 44,
+    USER_CREATED = 45,
+    ANONYMOUS_USER_CREATED = 46,
+  }
+}
+
---
+++ dist/include/page_pb.d.ts
@@ -0,0 +1,281 @@
+// package: apiv4.page
+// file: include/page.proto
+
+import * as jspb from "google-protobuf";
+import * as include_revision_pb from "../include/revision_pb";
+import * as include_user_pb from "../include/user_pb";
+
+export class Page_metadata extends jspb.Message {
+  getPageName(): string;
+  setPageName(value: string): void;
+
+  getNamespace(): string;
+  setNamespace(value: string): void;
+
+  getLanguageCode(): string;
+  setLanguageCode(value: string): void;
+
+  getPageIdBis(): string;
+  setPageIdBis(value: string): void;
+
+  getCreationTimeIso(): string;
+  setCreationTimeIso(value: string): void;
+
+  getUpdateTimeIso(): string;
+  setUpdateTimeIso(value: string): void;
+
+  getViewPermissionIdBis(): string;
+  setViewPermissionIdBis(value: string): void;
+
+  getLicenseName(): string;
+  setLicenseName(value: string): void;
+
+  getPageContentModel(): string;
+  setPageContentModel(value: string): void;
+
+  getEventIdBis(): string;
+  setEventIdBis(value: string): void;
+
+  getCreatorDisplayName(): string;
+  setCreatorDisplayName(value: string): void;
+
+  getCreatorIdName(): string;
+  setCreatorIdName(value: string): void;
+
+  getCurrentEditorDisplayName(): string;
+  setCurrentEditorDisplayName(value: string): void;
+
+  getCurrentEditorIdName(): string;
+  setCurrentEditorIdName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Page_metadata.AsObject;
+  static toObject(includeInstance: boolean, msg: Page_metadata): Page_metadata.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Page_metadata, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Page_metadata;
+  static deserializeBinaryFromReader(message: Page_metadata, reader: jspb.BinaryReader): Page_metadata;
+}
+
+export namespace Page_metadata {
+  export type AsObject = {
+    pageName: string,
+    namespace: string,
+    languageCode: string,
+    pageIdBis: string,
+    creationTimeIso: string,
+    updateTimeIso: string,
+    viewPermissionIdBis: string,
+    licenseName: string,
+    pageContentModel: string,
+    eventIdBis: string,
+    creatorDisplayName: string,
+    creatorIdName: string,
+    currentEditorDisplayName: string,
+    currentEditorIdName: string,
+  }
+}
+
+export class Page_with_rendering_elements extends jspb.Message {
+  hasPageMeta(): boolean;
+  clearPageMeta(): void;
+  getPageMeta(): Page_metadata | undefined;
+  setPageMeta(value?: Page_metadata): void;
+
+  hasRevMeta(): boolean;
+  clearRevMeta(): void;
+  getRevMeta(): include_revision_pb.Revision_metadata | undefined;
+  setRevMeta(value?: include_revision_pb.Revision_metadata): void;
+
+  getContent(): string;
+  setContent(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Page_with_rendering_elements.AsObject;
+  static toObject(includeInstance: boolean, msg: Page_with_rendering_elements): Page_with_rendering_elements.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Page_with_rendering_elements, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Page_with_rendering_elements;
+  static deserializeBinaryFromReader(message: Page_with_rendering_elements, reader: jspb.BinaryReader): Page_with_rendering_elements;
+}
+
+export namespace Page_with_rendering_elements {
+  export type AsObject = {
+    pageMeta?: Page_metadata.AsObject,
+    revMeta?: include_revision_pb.Revision_metadata.AsObject,
+    content: string,
+  }
+}
+
+export class Request_page_rendering_elements_by_name extends jspb.Message {
+  getPageName(): string;
+  setPageName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Request_page_rendering_elements_by_name.AsObject;
+  static toObject(includeInstance: boolean, msg: Request_page_rendering_elements_by_name): Request_page_rendering_elements_by_name.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Request_page_rendering_elements_by_name, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Request_page_rendering_elements_by_name;
+  static deserializeBinaryFromReader(message: Request_page_rendering_elements_by_name, reader: jspb.BinaryReader): Request_page_rendering_elements_by_name;
+}
+
+export namespace Request_page_rendering_elements_by_name {
+  export type AsObject = {
+    pageName: string,
+  }
+}
+
+export class Request_page_metadata_by_name extends jspb.Message {
+  getPageName(): string;
+  setPageName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Request_page_metadata_by_name.AsObject;
+  static toObject(includeInstance: boolean, msg: Request_page_metadata_by_name): Request_page_metadata_by_name.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Request_page_metadata_by_name, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Request_page_metadata_by_name;
+  static deserializeBinaryFromReader(message: Request_page_metadata_by_name, reader: jspb.BinaryReader): Request_page_metadata_by_name;
+}
+
+export namespace Request_page_metadata_by_name {
+  export type AsObject = {
+    pageName: string,
+  }
+}
+
+export class Request_create_or_edit_page extends jspb.Message {
+  getPageName(): string;
+  setPageName(value: string): void;
+
+  getContent(): string;
+  setContent(value: string): void;
+
+  getRevisionComment(): string;
+  setRevisionComment(value: string): void;
+
+  getVisibility(): Request_create_or_edit_page.VisibilityMap[keyof Request_create_or_edit_page.VisibilityMap];
+  setVisibility(value: Request_create_or_edit_page.VisibilityMap[keyof Request_create_or_edit_page.VisibilityMap]): void;
+
+  getLanguageCode(): string;
+  setLanguageCode(value: string): void;
+
+  getLicenseName(): string;
+  setLicenseName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Request_create_or_edit_page.AsObject;
+  static toObject(includeInstance: boolean, msg: Request_create_or_edit_page): Request_create_or_edit_page.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Request_create_or_edit_page, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Request_create_or_edit_page;
+  static deserializeBinaryFromReader(message: Request_create_or_edit_page, reader: jspb.BinaryReader): Request_create_or_edit_page;
+}
+
+export namespace Request_create_or_edit_page {
+  export type AsObject = {
+    pageName: string,
+    content: string,
+    revisionComment: string,
+    visibility: Request_create_or_edit_page.VisibilityMap[keyof Request_create_or_edit_page.VisibilityMap],
+    languageCode: string,
+    licenseName: string,
+  }
+
+  export interface VisibilityMap {
+    ALL_VISIBLE: 0;
+    ADMIN_VISIBLE: 1;
+    SELF_VISIBLE: 2;
+  }
+
+  export const Visibility: VisibilityMap;
+}
+
+export class Request_history_rendering_elements_by_page_name_ns extends jspb.Message {
+  getPageApparentName(): string;
+  setPageApparentName(value: string): void;
+
+  getDirection(): Request_history_rendering_elements_by_page_name_ns.DirectionMap[keyof Request_history_rendering_elements_by_page_name_ns.DirectionMap];
+  setDirection(value: Request_history_rendering_elements_by_page_name_ns.DirectionMap[keyof Request_history_rendering_elements_by_page_name_ns.DirectionMap]): void;
+
+  getStartFromMiddle(): boolean;
+  setStartFromMiddle(value: boolean): void;
+
+  hasRevIdBisNoninclusive(): boolean;
+  clearRevIdBisNoninclusive(): void;
+  getRevIdBisNoninclusive(): string;
+  setRevIdBisNoninclusive(value: string): void;
+
+  hasTimestampIsoNoninclusive(): boolean;
+  clearTimestampIsoNoninclusive(): void;
+  getTimestampIsoNoninclusive(): string;
+  setTimestampIsoNoninclusive(value: string): void;
+
+  getStartFromCase(): Request_history_rendering_elements_by_page_name_ns.StartFromCase;
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Request_history_rendering_elements_by_page_name_ns.AsObject;
+  static toObject(includeInstance: boolean, msg: Request_history_rendering_elements_by_page_name_ns): Request_history_rendering_elements_by_page_name_ns.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Request_history_rendering_elements_by_page_name_ns, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Request_history_rendering_elements_by_page_name_ns;
+  static deserializeBinaryFromReader(message: Request_history_rendering_elements_by_page_name_ns, reader: jspb.BinaryReader): Request_history_rendering_elements_by_page_name_ns;
+}
+
+export namespace Request_history_rendering_elements_by_page_name_ns {
+  export type AsObject = {
+    pageApparentName: string,
+    direction: Request_history_rendering_elements_by_page_name_ns.DirectionMap[keyof Request_history_rendering_elements_by_page_name_ns.DirectionMap],
+    startFromMiddle: boolean,
+    revIdBisNoninclusive: string,
+    timestampIsoNoninclusive: string,
+  }
+
+  export interface DirectionMap {
+    NEW_TO_OLD: 0;
+    OLD_TO_NEW: 1;
+  }
+
+  export const Direction: DirectionMap;
+
+  export enum StartFromCase {
+    START_FROM_NOT_SET = 0,
+    REV_ID_BIS_NONINCLUSIVE = 4,
+    TIMESTAMP_ISO_NONINCLUSIVE = 5,
+  }
+}
+
+export class Page_creation_report extends jspb.Message {
+  getSuccess(): boolean;
+  setSuccess(value: boolean): void;
+
+  getNewPidBis(): string;
+  setNewPidBis(value: string): void;
+
+  getNewApparentPageName(): string;
+  setNewApparentPageName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Page_creation_report.AsObject;
+  static toObject(includeInstance: boolean, msg: Page_creation_report): Page_creation_report.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Page_creation_report, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Page_creation_report;
+  static deserializeBinaryFromReader(message: Page_creation_report, reader: jspb.BinaryReader): Page_creation_report;
+}
+
+export namespace Page_creation_report {
+  export type AsObject = {
+    success: boolean,
+    newPidBis: string,
+    newApparentPageName: string,
+  }
+}
+
