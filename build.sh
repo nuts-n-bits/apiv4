2252f9d9d02aa77a8460207b70b37c751fed62a6
xiaoyu <xiaoyu@devDev3.local>
2020-04-28 17:10:03 -0400

    =

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 dist_dir="./dist"
-target_dir="../node_modules/apiv4"
+target_dir1="../node5tse/node_modules/apiv4"
+target_dir2="../angular2/node_modules/apiv4"
 
 echo "rm -rf" $dist_dir
 rm -rf $dist_dir
@@ -17,10 +18,14 @@ echo "protoc"
 "./nnb/user.proto" \
 "./nnb/feed.proto"
 
-echo "rm -rf" $target_dir
-rm -rf $target_dir
+echo "rm -rf" $target_dir1
+rm -rf $target_dir1
+echo "rm -rf" $target_dir2
+rm -rf $target_dir2
 
-echo "mv" $dist_dir $target_dir
-mv $dist_dir $target_dir
+echo "cp -r" $dist_dir $target_dir1
+cp -r $dist_dir $target_dir1
+echo "cp -r" $dist_dir $target_dir2
+cp -r $dist_dir $target_dir2
 
 echo "done"
